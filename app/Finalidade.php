71781206542998e2fdeda784d86f11bdcf18609f
Pedro Neto <pedromneto97@gmail.com>
2017-11-01 09:47:03 -0200

    Model Finalidade

---
+++ app/Finalidade.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Finalidade extends Model
+{
+    protected $table = 'finalidades';
+
+    protected $fillable = ['finalidade'];
+}
