c788a4371bed6a30e861f6f891083c53823e94c1
Pedro Neto <pedromneto97@gmail.com>
2017-11-22 16:56:19 -0200

    Novas rotas

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,22 @@
 */
 
 Route::resource('imovel', 'ImovelController');
+
 Route::resource('estado', 'EstadoController');
+
 Route::resource('rua', 'RuaController', ['except' => ['create', 'edit']]);
 Route::get('rua/cep/{cep}', 'RuaController@cep')->name('rua.cep');
+
 Route::resource('bairro', 'BairroController', ['except' => ['create', 'edit']]);
+Route::post('bairro/busca', 'BairroController@busca')->name('bairro.busca');
+
 Route::resource('cidade', 'CidadeController', ['except' => ['create', 'edit']]);
+Route::post('cidade/busca', 'CidadeController@busca')->name('cidade.busca');
+
 Route::resource('estado', 'EstadoController', ['except' => ['create', 'edit']]);
+
 Route::resource('interesse', 'InteresseController', ['except' => ['create', 'edit']]);
+
 Route::resource('tipo', 'TipoController', ['except' => ['create', 'edit']]);
+
 Route::resource('finalidade', 'InteresseController', ['except' => ['create', 'edit']]);
