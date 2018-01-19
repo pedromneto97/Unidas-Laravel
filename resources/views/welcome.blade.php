fbb96d51af31bc6841263ea31d5e6252827e181c
Pedro <pedromneto97@gmail.com>
2018-01-19 13:38:32 -0200

    Adaptação para build do angular

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,18 @@
 <!doctype html>
-<html lang="pt-BR">
+<html>
 <head>
     <meta charset="utf-8">
     <title>Imobiliária Unidas</title>
     <base href="/">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <link href="styles.b2cc8ddfefc29df11eee.bundle.css" rel="stylesheet"/>
+    <link href="styles.bundle.css" rel="stylesheet"/>
 </head>
 <body>
 <app-root>Carregando..</app-root>
-<script type="text/javascript" src="inline.64d1ce6a6df5dd7a0dde.bundle.js"></script>
-<script type="text/javascript" src="polyfills.dd2007432ec458ecea7e.bundle.js"></script>
-<script type="text/javascript" src="scripts.64e1a93274f0353ea812.bundle.js"></script>
-<script type="text/javascript" src="main.e064acf6828ad524fff6.bundle.js"></script>
+<script type="text/javascript" src="inline.bundle.js"></script>
+<script type="text/javascript" src="polyfills.bundle.js"></script>
+<script type="text/javascript" src="scripts.bundle.js"></script>
+<script type="text/javascript" src="main.bundle.js"></script>
 </body>
 </html>
