6c498a96f52994af85589c1180ab348205bc2a08
Pedro <pedromneto97@gmail.com>
2018-01-24 13:45:55 -0200

    Store do interesse

--- app/Http/Controllers/InteresseController.php
+++ app/Http/Controllers/InteresseController.php
@@ -31,18 +31,35 @@ class InteresseController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validator = Validator::make($request->all(), [
+                'nome' => 'string|min:3|max:60',
+                'telefone' => 'nullable|string',
+                'email' => 'nullable|string|email',
+                'id_imovel' => 'exists:imoveis,id'
+            ]);
+            if ($validator->fails())
+                return response($validator->errors(), 401);
+
+            $interesse = new Interesse();
+            $interesse->fill($request->all());
+            $interesse->atendido = false;
+            $interesse->save();
+            return response()->json($interesse);
+        } catch (\Exception $exception) {
+            return response($exception->getMessage(), 401);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Interesse  $interesse
+     * @param  \App\Interesse $interesse
      * @return \Illuminate\Http\Response
      */
     public function show(Interesse $interesse)
@@ -53,7 +70,7 @@ class InteresseController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Interesse  $interesse
+     * @param  \App\Interesse $interesse
      * @return \Illuminate\Http\Response
      */
     public function edit(Interesse $interesse)
@@ -64,8 +81,8 @@ class InteresseController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Interesse  $interesse
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Interesse $interesse
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Interesse $interesse)
@@ -76,7 +93,7 @@ class InteresseController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Interesse  $interesse
+     * @param  \App\Interesse $interesse
      * @return \Illuminate\Http\Response
      */
     public function destroy(Interesse $interesse)
